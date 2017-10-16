8b7a8d6a84a368d9d194ab12179fef11a9e36663
Chris B <cbski4@live.com>
2017-10-15 20:04:48 -0500

    Live Editor blueprint

--- live_editor.js
+++ live_editor.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  
+
 });
+
+
+/*
+
+  1. Add Nav bar for HTML/CSS/JS/Output
+  2. Click listeners to add borders/hide
+  3. Make Writeable windows
+  4. one master string written to from each window
+  
+
+*/
